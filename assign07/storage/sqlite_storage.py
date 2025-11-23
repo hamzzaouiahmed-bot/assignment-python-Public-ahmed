103f589b36ab148790ecb006bbcf8ef69a62b8ed
Ato <your_email@example.com>
2025-11-23 18:55:59 +0100

    Delete assign07 folder

--- assign07/storage/sqlite_storage.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-Base = declarative_base()
-engine = create_engine("sqlite:///ahmed.db", echo=True)
-SessionLocal = sessionmaker(bind=engine)
